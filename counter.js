8f21c5cafa38dc8ce9a7314161ba8348e027c074
pppog <loke.oberg@elev.ga.ntig.se>
2023-05-09 15:10:40 +0200

    allt

--- counter.js
+++ counter.js
@@ -1,18 +1,7 @@
-export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
-}
-
 export function doFunction() {
   if(document.getElementById("answertext").innerText == document.getElementsByTagName("input")[0].value) {
     alert("you won")
   }
-  guessnum = searchnum
   guessnum = searchnum;
     console.log(guessnum)
     guess = document.getElementsByTagName("input")[0].value;
--- javascript.js
+++ javascript.js
@@ -1,49 +1,91 @@
-import { myFunction } from './counter.js'
-import { doFunction } from './counter.js'
-import { searchItem } from './counter.js'
-  import items from './itemslist.json'
-  window.myFunction = myFunction
-  window.doFunction = doFunction
-  window.searchItem = searchItem
-  
-  
-//let boots = items.data.filter(item => item.tags.indexOf('boots') > 0);
-let purchasableitems = [];
-for (const [key, value] of Object.entries(items.data)) {
-    //console.log(`${key}: ${value.name}`);
-    console.log(value)
-    if (value.gold.purchasable == 1) {
-        purchasableitems.push(value);
+function searchItem(name) {
+  document.getElementById("myInput").value = name;
+}
+
+function doFunction() {
+    guess = document.getElementsByTagName("input")[0].value;
+    let guesses = document.getElementById("printGuesses").innerHTML.charAt((document.getElementById("printGuesses").innerHTML.indexOf("made")+5))
+       if (document.getElementById("printGuesses").innerHTML == "") {
+      document.getElementById("printGuesses").innerHTML = "You have made " + 1 + " guess.";
+      guesses = 1;
+    } else {
+      guesses++;
+    document.getElementById("printGuesses").innerHTML = "You have made " + guesses + " guesses.";
+    }
+    let hints = document.getElementById("hints");
+    let hint = document.createElement("p");
+hints.append(hint);
+if (guesses == 1) {
+hint.innerText += document.getElementById("cost").innerHTML;
+} else if (guesses == 2) {
+  hint.innerText += document.getElementById("tags").innerHTML;
+} else if (guesses == 3) {
+  hint.innerText += document.getElementById("description").innerHTML;
+} else if (guesses == 4) {
+  hint.innerText += document.getElementById("passive").innerHTML;
+} 
+      
+    console.log("Your guess: " + guess);
+    console.log("guesses = " + guesses)
+    if (guess.toLowerCase() === (document.getElementById("name").innerHTML).toLowerCase()) {
+        alert("You won!\nThe item was: " + document.getElementById("name").innerHTML + "\nYou did it in " + guesses + " guesses.");
     }
 }
-console.log(purchasableitems)
-  
-  let olditems = {
-      mythics: {
-          int: [0,1,2,3,4,5,6],
-          name: ["Goredrinker","Duskblade","Liandrys","Prowlers","Jak'Sho","Heartsteel","Evenshroud"],
-          cost: [3300,3100,3200,3100,3200,],
-          ad: [1,1,0,1,0,0,0],
-          ap: [0,0,1,0,0,0,0],
-          hp: [1,0,0,0,1,1,1],
-          ah: [1,1,1,1,1,1,1],
-          armor: [0,0,0,0,1,0,1],
-          mr: [0,0,0,0,1,0,1],
-      }
-  };
+
+function myFunction() {
+  // Declare variables
+  let input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName("li");
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+const createList = (items) => {
   let list = document.getElementById("myUL");
   let searchnum = 0;
-  for (const [key, value] of Object.entries(purchasableitems)) {
+  for (const [key, value] of Object.entries(items)) {
+    let li = document.createElement("li");
+    let a = document.createElement("a");
+    
+    a.innerText = value.name;
+    a.setAttribute(
+      "onclick",
+      'searchItem("' + value.name + '");'
+    );
+    list.appendChild(li);
+    li.appendChild(a);
+  }
+};
 
-      let li = document.createElement("li");
-      let a = document.createElement("a");
-      
-      a.innerText = value.name;
-      a.setAttribute('onclick','searchItem("' + value.name + '",' + 0 + ',' + searchnum + ');')
-      list.appendChild(li);
-      li.appendChild(a);
-    }
+
+
+const setup = async () => {
+  const res = await fetch("/itemslist.json");
+  const data = await res.json();
   
+  let purchasableitems = [];
+  
+  for (const [key, value] of Object.entries(data.data)) {
+    //console.log(value)
+    if (value.gold.purchasable == 1) {
+      purchasableitems.push(value);
+    }
+  }
+
+  //  return purchasableitems;
+  
+  createList(purchasableitems)
   
   
   
@@ -52,9 +94,18 @@ console.log(purchasableitems)
   let guess = "";
   let itemnum = Math.round(Math.random() * 192);
   let answer = purchasableitems[itemnum];
+  let passive = "";
+  for(let i = answer.description.indexOf("<passive>")+9; i<answer.description.indexOf("</passive>")-1;i++) {
+ passive += answer.description.charAt(i);
+  }
   
-  console.log(answer)
-  
-  document.getElementById("answertags").innerText = answer.tags
-  document.getElementById("answergold").innerText = answer.gold.total
-  document.getElementById("answertext").innerText = answer.name
+  document.getElementById("name").innerHTML = answer.name;
+  document.getElementById("tags").innerHTML = "tags: " + answer.tags;
+  document.getElementById("passive").innerHTML = "passive: " + passive;
+  document.getElementById("description").innerHTML = "description: " + answer.plaintext;
+  document.getElementById("cost").innerHTML = "cost: " + answer.gold.total;
+
+  console.log(answer);
+};
+
+setup();
